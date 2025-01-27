635dfb31bfee9a7304f63c43ac4092f006345235
princy2352 <princyy2352@gmail.com>
2025-01-27 02:21:19 -0500

    Adding edited sreamlit file

--- ResearchAgent/streamlit_app.py
+++ ResearchAgent/streamlit_app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# Add the project root to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.model.google import Gemini 
